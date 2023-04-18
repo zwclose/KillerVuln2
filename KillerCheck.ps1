e1c7470ce2fe108d8a214dcbc82f3e0486460f27
zwclose <zwclose@users.noreply.github.com>
2023-04-18 21:19:51 +0300

    Add KillerCheck.ps1

---
+++ KillerCheck.ps1
@@ -0,0 +1,49 @@
+﻿#
+# The script tries to check if the Killer's COM server is vulnerable.
+# The script is just a hand-made utility and does not imply any warranty.
+#
+
+try
+{
+  $clsid = New-Object Guid '7972960B-C3EE-4794-B28B-75F9D36760E6'
+  $type = [Type]::GetTypeFromCLSID($clsid)
+  $object = [Activator]::CreateInstance($type)
+
+  try
+  {
+    $object.Initialize(5, 0, 0, "", "")
+    Write-Host "Looks vulnerable"
+  }
+  catch [System.Runtime.InteropServices.COMException]
+  {
+    if ($_.Exception.ErrorCode -eq 0x80010110)
+    {
+      try
+      {
+        $object.Initialize(6, 0, 0, "", "")
+      }
+      catch [System.Runtime.InteropServices.COMException]
+      {
+        if ($_.Exception.ErrorCode -eq 0x8001011B)
+        {
+          Write-Host "Looks patched"
+        }
+        else
+        {
+          Write-Host "Unexpected exception"
+        }
+      }
+    }
+  }
+}
+catch [System.Runtime.InteropServices.COMException]
+{
+  if ($_.Exception.ErrorCode -eq 0x80070424)
+  {
+    Write-Host "Killer COM server not registered"
+  }
+  else
+  {
+    Write-Host "Unexpected exception"
+  }
+}
--- README.md
+++ README.md
@@ -4,6 +4,7 @@ This repository contains supporting files for the PoC of a vulnerability in the 
 * WindowsUpdate.ps1: blocks network access for all svchost.exe processes in the OS. As result, Windows update stops working
 * Intel.ps1: blocks access to intel.com
 * Services.ps1: enables and starts remote registry and rdp services, disables and stops volume shadow copy service
-* KillerNetworkService.idl: MIDL of Killer's COM server
+* KillerNetworkService.idl: IDL of Killer's COM server
+* KillerCheck.ps1: checks whether Killer's COM server is patched or not
 
 For details of the vulnerability and the demo video refer to the [blogpost](https://zwclose.github.io/). Feel free to ask questions in [Twitter](https://twitter.com/zwclose)
